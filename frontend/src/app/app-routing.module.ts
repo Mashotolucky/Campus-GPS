efb62319fac0a6b22a5edba2b2c90d89cf5eb20b
Mashotolucky <mashotolucky99@gmail.com>
2023-03-10 21:39:48 +0200

    Admin dashboard UI

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminHomeComponent } from './pages/admin-home/admin-home.component';
+import { AdminComponent } from './pages/admin/admin.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 
 const routes: Routes = [
   {path: '', component: LoginComponent},
-  {path: 'register', component: RegisterComponent}
+  {path: 'register', component: RegisterComponent},
+  {
+  path: 'admin', component: AdminComponent,
+    children: [
+      { path: '', component: AdminHomeComponent }
+    ],
+  }
 ];
 
 @NgModule({
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,12 +8,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
+import { LogoutModalComponent } from './components/logout-modal/logout-modal.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { TopbarComponent } from './components/topbar/topbar.component';
+import { AdminHomeComponent } from './pages/admin-home/admin-home.component';
+import { AdminComponent } from './pages/admin/admin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    AdminDashboardComponent,
+    LogoutModalComponent,
+    SidebarComponent,
+    TopbarComponent,
+    AdminHomeComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
