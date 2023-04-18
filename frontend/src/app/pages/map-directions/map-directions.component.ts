203362fc37524b8aeed751429fd24be496c2509f
Mashotolucky <mashotolucky99@gmail.com>
2023-04-18 20:15:44 +0200

    differiate position by area poly--

--- frontend/src/app/pages/map-directions/map-directions.component.ts
+++ frontend/src/app/pages/map-directions/map-directions.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { log } from 'console';
 import * as L from 'leaflet';
 import  'leaflet-routing-machine';
+import 'leaflet-providers';
+
 // import 'leaflet-geometryutil';
 import { getDistance } from 'geolib';
 
@@ -30,16 +32,16 @@ export class MapDirectionsComponent implements OnInit {
 
   ngOnInit(): void {
     
+    this.testPoint();
 
     this.map = L.map('map').setView([-25.54050582, 28.096141], 17);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 18
     }).addTo(this.map);
 
-    
-    
-    this.alternativeRoute();
+    // this.alternativeRoute();
     // this.mainRoute();
+    // this.getFastestRoute();
 
     const clickedCoords: { lat: any; lng: any; }[] = [];
 
@@ -54,10 +56,8 @@ export class MapDirectionsComponent implements OnInit {
         console.log("Clicked coordinates:", clickedCoords);
     });
 
-    this.distanceAPI();
-
   }
-
+  // added marker to destination 
   addMaker(map: any, latlng: any): void{
     this.marker = L.marker([latlng.lat, latlng.lng]).addTo(map)
   }
@@ -69,6 +69,8 @@ export class MapDirectionsComponent implements OnInit {
       const nameBody = {name: name}
 
       const routeArr: any[] = [];
+      const alternativeArr: any[] = [];
+
       // Do something with the name parameter
       this.mapService.getLocationByName(nameBody)
       .subscribe(res => {
@@ -83,6 +85,10 @@ export class MapDirectionsComponent implements OnInit {
         const waypoints = routeArr[0];
         const latLngArray = waypoints.map(({ lat, lng }: { lat: number, lng: number }) => ({ lat, lng }));
 
+        alternativeArr.push(this.mapData.alternative.data.alternatives);
+        const alternatives = alternativeArr[0];
+        const altlatLngArray = alternatives.map(({ lat, lng }: { lat: number, lng: number }) => ({ lat, lng }));
+
         const location = this.mapData.mainroute.data.location[0];
 
         const latLng = {
@@ -91,6 +97,8 @@ export class MapDirectionsComponent implements OnInit {
         };
 
         latLngArray.push(latLng);
+        altlatLngArray.push(latLng);
+
         console.log(latLngArray);
 
         
@@ -110,18 +118,16 @@ export class MapDirectionsComponent implements OnInit {
               const lng = position.coords.longitude;
       
               latLngArray.unshift({lat: lat, lng: lng});
+              altlatLngArray.unshift({lat: lat, lng: lng});
+
               console.log(latLngArray);
-              // Update the map with the new position
-              // this.updateMap(lat, lng);
+
+              this.makeRoute(latLngArray);
+              
             });
           } else {
             console.log('Geolocation is not supported by this browser.');
           }
-
-          
-
-          this.makeRoute(latLngArray);
-          this.calculateDistance(latLngArray);
 
           let radius = e.accuracy / 340 ;
           let circle = L.circle(e.latlng, {
@@ -235,7 +241,6 @@ export class MapDirectionsComponent implements OnInit {
           
 
           this.makeRoute(latLngArray);
-          this.calculateDistance(latLngArray);
 
           let radius = e.accuracy / 340 ;
           let circle = L.circle(e.latlng, {
@@ -294,23 +299,176 @@ export class MapDirectionsComponent implements OnInit {
 
   }
 
-  distanceAPI(){
-    this.mapService.getDistanceAPI()
-    .subscribe(res =>{
-      console.log('distance API',res);
-      
-    })
+  calculateDistance(wayponits: any, alternatives: any){
+    console.log(wayponits);
+    console.log(alternatives);
+    
+    
+    const mainCoord1 = { latitude: wayponits[0].lat, longitude: wayponits[0].lng }; 
+    const mainCoord2 = { latitude: wayponits[1].lat, longitude: wayponits[1].lng };
+
+    const mainDistance = getDistance(mainCoord1, mainCoord2);
+
+    console.log('Main Distance',mainDistance);
+
+    const altCoord1 = { latitude: alternatives[0].lat, longitude: alternatives[0].lng }; 
+    const altCoord2 = { latitude: alternatives[1].lat, longitude: alternatives[1].lng };
+
+    const altDistance = getDistance(altCoord1, altCoord2);
+    console.log('Alt Distance',altDistance);
+    
+    if(altDistance > mainDistance){
+      this.mainRoute();
+    }
+    else{
+      this.alternativeRoute();
+    }
+
+    
   }
 
-  calculateDistance(wayponits: any){
-    console.log(wayponits);
+  // getFastestRoute(): void{
+  //   this.route.queryParams.subscribe(params => {
+  //     const name = params['name'];
+  //     console.log('location name', name);
+  //     const nameBody = {name: name}
+
+  //     const mainArr: any[] = [];
+  //     const alternativeArr: any[] = [];
+
+  //     // Do something with the name parameter
+  //     this.mapService.getLocationByName(nameBody)
+  //     .subscribe(res => {
+  //       console.log(res);
+        
+  //       this.mapData = res;
+  //       console.log(this.mapData.mainroute.data);
+  //       mainArr.push(this.mapData.mainroute.data.waypoints);
+
+  //       alternativeArr.push(this.mapData.alternative.data.alternatives);
+  //       const alternatives = alternativeArr[0];
+  //       const altlatLngArray = alternatives.map(({ lat, lng }: { lat: number, lng: number }) => ({ lat, lng }));
+
+  //       const waypoints = mainArr[0];
+  //       const mainlatLlng: ngArray = waypoints.map(({ lat, lng }: { lat: number, lng: number }) => ({ lat, lng }));
+
+  //       //distination coordinates
+  //       const location = this.mapData.mainroute.data.location[0];
+
+  //       const latLng = {
+  //         lat: location.lat,
+  //         lng: location.lng
+  //       };
+
+  //       mainlatLngArray.push(latLng);
+  //       altlatLngArray.push(latLng);
+
+  //       console.log(mainlatLngArray);
+
+        
+  //       this.arraylist = mainlatLngArray;
+        
+  //       //  Get Current live location
+  //       this.map.locate({setView: true});
+  //       this.map.on('locationfound', (e: any) => {
+  //         console.log(e.latlng);
+
+  //         if (navigator.geolocation) {
+  //           navigator.geolocation.watchPosition((position) => {
+  //             const lat = position.coords.latitude;
+  //             const lng = position.coords.longitude;
+      
+  //             mainlatLngArray.unshift({lat: lat, lng: lng});
+  //             console.log(mainlatLngArray);
+
+  //             altlatLngArray.unshift({lat: lat, lng: lng});
+  //             console.log(altlatLngArray);
+              
+  //             // Calculate distance
+  //             this.calculateDistance(mainlatLngArray, altlatLngArray);
+  //           });
+  //         } else {
+  //           console.log('Geolocation is not supported by this browser.');
+  //         }
+  //       });
+  //     })
+  //   });
+  // }
+
+  isPointInPolygon(point: any, polygon: any) {
+    let inside = false;
+    for (let i = 0, j = polygon.length - 1; i < polygon.length; j = i++) {
+      const xi = polygon[i][0], yi = polygon[i][1];
+      const xj = polygon[j][0], yj = polygon[j][1];
+      const intersect = ((yi > point[1]) != (yj > point[1])) &&
+        (point[0] < (xj - xi) * (point[1] - yi) / (yj - yi) + xi);
+      if (intersect) inside = !inside;
+    }
+    return inside;
+  }
+
+  testPoint(): void{
+
+    //  Get Current live location
+    // this.map.locate({setView: true});
+    // this.map.on('locationfound', (e: any) => {
+    //   console.log(e.latlng);
+
+    //   if (navigator.geolocation) {
+    //     navigator.geolocation.watchPosition((position) => {
+    //       const lat = position.coords.latitude;
+    //       const lng = position.coords.longitude;
+  
+    //     });
+    //   } else {
+    //     console.log('Geolocation is not supported by this browser.');
+    //   }
+    // });
+
+    // Define the polygons
+    const polygon1 = [[-25.54050540139702, 28.095023632049564], [-25.540471519915943, 28.09564590454102], [-25.539890693037382, 28.09558153152466], [-25.539847130908075, 28.09505045413971]];
+    const polygon2 = [[5, 5], [5, 15], [15, 15], [15, 5]];
+
+    // Test each point in the polygons and differentiate the areas
+    // const area1 = [], area2 = [];
+    var isFound = false;
+
+    const point = [-25.540292431928407, 28.095828294754032];
+      if (this.isPointInPolygon(point, polygon1)) {
+        isFound = true;
+        
+        this.mainRoute();
+      } else {
+        // area2.push(point);
+        isFound = false;
+        this.alternativeRoute();
+        
+      }
+
+    // for (let i = 0; i < polygon1.length; i++) {
+    //   const point = [-25.540365035198835, 28.095425963401798];
+    //   if (this.isPointInPolygon(point, polygon1)) {
+    //     area1.push(point);
+    //   } else {
+    //     area2.push(point);
+    //   }
+    // }
+
     
-    const coord1 = { latitude: wayponits[0].lat, longitude: wayponits[0].lng }; // San Francisco
-    const coord2 = { latitude: wayponits[1].lat, longitude: wayponits[1].lng };
+    // for (let i = 0; i < polygon2.length; i++) {
+    //   const point = polygon2[i];
+    //   if (this.isPointInPolygon(point, polygon2)) {
+    //     area1.push(point);
+    //   } else {
+    //     area2.push(point);
+    //   }
+    // }
 
-    const distance = getDistance(coord1, coord2);
-
-    console.log('Geo Distance',distance);
+    // Print the differentiated areas
+    // console.log('Area 1:', area1);
+    // console.log('Area 2:', area2);
+    console.log('is Found = ', isFound);
+    
   }
 
 }
